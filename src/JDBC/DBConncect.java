a8e803cec3ecdd8120f86b062db25a1478214594
mehmetaliesgi <mehmetaliesgi@gmail.com>
2023-12-30 16:15:15 +0300

    Database bağlantısı ve JDBC hakkında bilgi edinildi.

---
+++ src/JDBC/DBConncect.java
@@ -0,0 +1,31 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConncect {
+    public static final String DB_URL = "jdbc:postgresql:PatikaDev";
+    public static final String DB_USER = "postgres";
+    public static final String DB_PASSWORD = "root";
+    public static void main(String[] args) {
+        /* Bu şekilde bir işlem ile DB bağlantısı kurulabilir ama burada URL deki değişkenleri bir değişkende tutmak daha mantıklı olur.
+        Connection connection = null;
+        try {
+            String url = "jdbc:postgresql:PatikaDev?user=postgres&password=root";
+            connection = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        */
+
+        // Bu şekilde kullanımı daha yakışıklı :)
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            System.out.println("Database connection is successfully!");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
