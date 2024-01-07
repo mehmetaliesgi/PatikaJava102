eef48da2d542fd943f5712bc8a486b6a94797dfb
mehmetaliesgi <mehmetaliesgi@gmail.com>
2024-01-08 00:20:53 +0300

    Book classından oluşturulan nesneler üzerinde lambda expressionlar ve steam api kullanılarak işlemler yapıldı.

---
+++ src/BookListHW/Book.java
@@ -0,0 +1,47 @@
+package BookListHW;
+
+public class Book {
+    private String bookName;
+    private int pageNumber;
+    private String authorName;
+    private String publicationDate;
+
+    public Book(String bookName, int pageNumber, String authorName, String publicationDate) {
+        this.bookName = bookName;
+        this.pageNumber = pageNumber;
+        this.authorName = authorName;
+        this.publicationDate = publicationDate;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(String publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+}
---
+++ src/BookListHW/Main.java
@@ -0,0 +1,36 @@
+package BookListHW;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList <Book> bookArrayList = new ArrayList<>();
+        HashMap <String, String> bookNameAndAuthor = new HashMap<>();
+        ArrayList <Book> moreThan100Page = new ArrayList<>();
+
+        for (int i = 1; i <= 4; i++) {
+            System.out.print("Lütfen " + i + ". kitap ismini giriniz: ");
+            String bookName = scanner.next();
+            System.out.print("Lütfen " + bookName + " kitabının sayfa sayısını giriniz: ");
+            int pageNumber = scanner.nextInt();
+            System.out.print("Lütfen yazarın ismini giriniz: ");
+            String author = scanner.next();
+            System.out.print("Lütfen kitabın yayım tarihini giriniz (dd.mm.yyyy): ");
+            String publishDate = scanner.next();
+            System.out.println("---------------------------------------------------");
+            bookArrayList.add(new Book(bookName, pageNumber, author, publishDate));
+        }
+
+        bookArrayList.stream().forEach(book -> {
+            bookNameAndAuthor.put(book.getBookName(), book.getAuthorName());
+        });
+
+        bookArrayList.stream().filter(book -> book.getPageNumber() > 100).forEach(book -> {
+            moreThan100Page.add(book);
+        });
+    }
+}
